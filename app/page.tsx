127d7b9978abb0812c12a4f60bbde066a3b8125a
AJ <silva.andrick2001@gamil.com>
2023-09-27 14:50:45 +0530

    fixed styles

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,9 @@ export default function Home() {
     <main className="flex min-h-screen  items-center justify-between">
       <div
         className={`w-full min-h-screen ${
-          true ? `bg-blue-400` : `bg-yellow-100`
+          data?.current?.current?.is_day == "no"
+            ? `bg-blue-400`
+            : `bg-yellow-100`
         } flex justify-center items-center flex-col relative`}
       >
         <p className="text-sm text-slate-900 absolute top-10">
@@ -38,7 +40,7 @@ export default function Home() {
             ? data?.current?.observation_time
             : "12/02/23"}
         </p>
-        {true ? (
+        {data?.current?.current?.is_day == "no" ? (
           <Image
             src="/moon.svg"
             alt="weather icon"
@@ -57,7 +59,9 @@ export default function Home() {
         )}
         <p
           className={`text-6xl font-bold ${
-            true ? `text-blue-800` : `text-orange-400`
+            data?.current?.current?.is_day == "no"
+              ? `text-blue-800`
+              : `text-orange-400`
           } absolute bottom-32`}
         >
           {data?.current?.temperature
@@ -66,7 +70,9 @@ export default function Home() {
         </p>
         <p
           className={`text-3xl  ${
-            true ? `text-blue-100` : `text-orange-900`
+            data?.current?.current?.is_day == "no"
+              ? `text-blue-100`
+              : `text-orange-900`
           } absolute bottom-20`}
         >
           {data?.current?.weather_descriptions[0]
@@ -76,7 +82,9 @@ export default function Home() {
       </div>
       <div
         className={`w-full min-h-screen flex justify-center items-center flex-col gap-10 ${
-          true ? `bg-blue-100` : `bg-orange-50`
+          data?.current?.current?.is_day == "no"
+            ? `bg-blue-100`
+            : `bg-orange-50`
         }`}
       >
         <form onSubmit={handleSubmit} className="relative w-1/2">
@@ -86,13 +94,17 @@ export default function Home() {
             onChange={(e) => setChange(e.target.value)}
             placeholder="Enter Country name"
             className={`px-6 py-2 rounded-full border ${
-              true ? `border-blue-800` : `border-orange-400`
+              data?.current?.current?.is_day == "no"
+                ? `border-blue-800`
+                : `border-orange-400`
             } w-full`}
           />
           <button
             type="submit"
             className={`${
-              true ? `bg-blue-800` : `bg-yellow-400`
+              data?.current?.current?.is_day == "no"
+                ? `bg-blue-800`
+                : `bg-yellow-400`
             } text-white px-6 h-full rounded-full absolute top-0 right-0`}
           >
             Submit
@@ -102,14 +114,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Wind speed
             </p>
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.wind_speed ? data?.current?.wind_speed : "19"}
@@ -118,14 +134,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Wind degree
             </p>
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.wind_degree ? data?.current?.wind_degree : "29"}
@@ -134,14 +154,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Wind direction
             </p>
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.wind_dir ? data?.current?.wind_dir : "North"}
@@ -150,14 +174,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Pressure
             </p>{" "}
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.pressure ? data?.current?.pressure : "10"}
@@ -166,14 +194,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               precip
             </p>{" "}
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.precip ? data?.current?.precip : "19"}
@@ -182,14 +214,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Humidity
             </p>{" "}
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.humidity ? data?.current?.humidity : "18"}
@@ -198,14 +234,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Cloud cover
             </p>
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.cloudcover ? data?.current?.cloudcover : "19"}
@@ -214,14 +254,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Feels like
             </p>
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.feelslike ? data?.current?.feelslike : "12%"}
@@ -230,14 +274,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               UV index
             </p>
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.uv_index ? data?.current?.uv_index : "14"}
@@ -246,14 +294,18 @@ export default function Home() {
           <div className="flex items-center justify-between w-1/2 ">
             <p
               className={`text-xl ${
-                true ? `text-blue-800` : `text-orange-400`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-800`
+                  : `text-orange-400`
               }`}
             >
               Visibility
             </p>{" "}
             <span
               className={`text-2xl font-bold ${
-                true ? `text-blue-900` : `text-orange-900`
+                data?.current?.current?.is_day == "no"
+                  ? `text-blue-900`
+                  : `text-orange-900`
               }`}
             >
               {data?.current?.visibility ? data?.current?.visibility : "4"}
